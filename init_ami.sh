e30100807343b09db4c0a5cfb6d6ec8aff146395
davluangu <dxdlnxn@gmail.com>
2016-01-23 20:39:46 -0800

    docker port map

--- init_ami.sh
+++ init_ami.sh
@@ -4,7 +4,8 @@ curl -sSL https://get.docker.com/ | sh
 sudo usermod -aG docker ubuntu # Remember that you will have to log out and back in for this to take effect!
 
 docker pull davluangu/mlapi
-docker run -i -t davluangu/mlapi /bin/bash
+docker run -i -t -p 80:5000 davluangu/mlapi /bin/bash
+
 
 conda update conda -y
 conda create --name mlapp flask -y
